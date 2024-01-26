23cf3be40ddcfa071590b918738fcfc47c91c762
Dipto2611 <diptaraj.ch.26@gmail.com>
2024-01-26 13:35:41 +0530

    Just started

---
+++ hello.cpp
@@ -0,0 +1,26 @@
+// C++ program to display "Hello World"
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	cout << "Hello World";
+
+	return 0;
+}
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"Hi new to C++??" << endl; //to print we use the quotations
+//     cout<< "No worries" << endl;
+//     cout<< "I will teach you how to code, hehe" << endl;
+
+//     return 0;
+// }
